30df5c6f33e3c71b0ee16ef15cac03b158c97915
Maryem <61624726+Maryem2499@users.noreply.github.com>
2022-08-12 17:47:34 +0100

    Add files via upload

---
+++ question_model.py
@@ -0,0 +1,8 @@
+# TODO: 1. Create a class Question
+class Question:
+    # Initilize attributes
+    def __init__(self, text_q, answer_q):
+        self.text = text_q
+        self.answer = answer_q
+
+
